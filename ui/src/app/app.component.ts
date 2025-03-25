c717690769f2693543555d3cbea4ef55d8f2be3a
Maciej Sielski <Maciej.Sielski@ey.com>
2025-03-25 16:12:37 +0100

    menu with slider

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, Signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import { AuthService } from './auth';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, ],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
